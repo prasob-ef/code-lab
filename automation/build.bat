bc24fa0597d0addb61d4652b23315e15a803a078
prasob-ef <prasob.kumar@ef.com>
2019-06-27 23:08:10 +0530

    Update build.bat

--- automation/build.bat
+++ automation/build.bat
@@ -1,8 +1,8 @@
 npm install
 npm run prod
 
-C:\Windows\System32\inetsrv\appcmd stop apppool /apppool.name:"betamyqa.ef.com"
-ren "D:\websites\betamyqa.ef.com\bundle.js" "bundle_bkp-%date:~10,4%%date:~7,2%%date:~4,2%-%time:~0,2%%time:~3,2%.js"
-copy "D:\Jenkins\workspace\betamyqa.ef.com\build\bundle.js" "D:\websites\betamyqa.ef.com\bundle.js"
+C:\Windows\System32\inetsrv\appcmd stop apppool /apppool.name:"code-lab.localhost.com"
 
-C:\Windows\System32\inetsrv\appcmd start apppool /apppool.name:"betamyqa.ef.com"
+copy "C:\Program Files (x86)\Jenkins\workspace\code-lab.localhost.com" *.* "F:\_Localhost\code-lab.localhost.com"
+
+C:\Windows\System32\inetsrv\appcmd start apppool /apppool.name:"code-lab.localhost.com"
