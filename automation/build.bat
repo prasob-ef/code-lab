9598059be0be63603b5f35559e99a93be8d52a88
prasob-ef <prasob.kumar@ef.com>
2019-06-28 00:42:47 +0530

    Update build.bat

--- automation/build.bat
+++ automation/build.bat
@@ -1,8 +1,15 @@
-npm install
-npm run prod
+@echo on
+echo installing node modules...
+call npm install || goto :error
+echo building project..
+call npm run build || goto :error
+echo stopping code-lab.localhost.com app pool..
+call C:\Windows\System32\inetsrv\appcmd stop apppool /apppool.name:"code-lab.localhost.com" || goto :error
+echo copying file to F:\_Localhost\code-lab.localhost.com
+call xcopy "C:\Program Files (x86)\Jenkins\workspace\code-lab_develop\build" "F:\_Localhost\code-lab.localhost.com\" /y || goto :error
+call attrib -r +s drive: F:\_Localhost\code-lab.localhost.com\*.* /s
+echo starting code-lab.localhost.com app pool..
+call C:\Windows\System32\inetsrv\appcmd start apppool /apppool.name:"code-lab.localhost.com" || goto :error
 
-C:\Windows\System32\inetsrv\appcmd stop apppool /apppool.name:"code-lab.localhost.com"
-
-copy "C:\Program Files (x86)\Jenkins\workspace\code-lab.localhost.com" *.* "F:\_Localhost\code-lab.localhost.com"
-
-C:\Windows\System32\inetsrv\appcmd start apppool /apppool.name:"code-lab.localhost.com"
+:error
+exit /b %errorlevel%
